4fd5e6545851a54b281b5166d6f28e34f6839d40
Jarod997 <Pretender42@gmail.com>
2024-10-02 23:46:20 -0400

    Added bot startup timestamp in console log and in Discord post.

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Events } = require('discord.js');
 
 // Say hi to the log file when the bot wakes up
@@ -5,7 +6,10 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Logged in as ${client.user.tag} at ${Date().toString()}`);
         console.log(`✅ ${client.user.tag} - ${client.user.username} is online.`);
+		// Post to the channel when the bot (last) started
+		const startupMessage = `✅ ${client.user.username} is online.`;
+		client.channels.cache.get(process.env.CHANNELID).send(startupMessage);
 	},
 };
